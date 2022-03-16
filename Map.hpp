4f2db2c9c51aebc414ce40ed44e0ac0d84a712c9
Gabriel Asselin <gasselin@c2r4p7.42quebec.com>
2022-03-16 17:19:13 -0400

    3rd day

--- Map.hpp
+++ Map.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Map.hpp                                            :+:      :+:    :+:   */
+/*   map.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gasselin <gasselin@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 14:48:23 by gasselin          #+#    #+#             */
-/*   Updated: 2022/03/14 15:03:46 by gasselin         ###   ########.fr       */
+/*   Updated: 2022/03/16 13:03:02 by gasselin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,89 @@
 
 namespace ft
 {
-	template < class Key, class T, class Compare = less<Key>, class Alloc = std::allocator<ft::pair<const Key,T> > >
+	template < class Key, class T, class Compare = ft::less<Key>, class Alloc = std::allocator<ft::pair<const Key,T> > >
 	class map
 	{
+		public:
+			typedef				Key 													key_type;
+			typedef				T 														mapped_type;
+			typedef				ft::pair<const key_type, mapped_type> 					value_type;
+			typedef				Compare 												key_compare;
+			typedef				Alloc 													allocator_type;
+			typedef typename	allocator_type::reference 								reference;
+			typedef typename	allocator_type::const_reference 						const_reference;
+			typedef typename	allocator_type::pointer 								pointer;
+			typedef typename	allocator_type::const_pointer 							const_pointer;
+			typedef				ft::bidir_iterator<value_type> 							iterator;
+			typedef				ft::const_bidir_iterator<value_type> 					const_iterator;
+			typedef				ft::reverse_iterator<bidir_iterator> 					reverse_iterator;
+			typedef				ft::reverse_iterator<const_bidir_iterator> 				const_reverse_iterator;
+			typedef typename	ft::iterator_traits<bidir_iterator>::difference_type	difference_type;
+			typedef typename	allocator_type::size_type								size_type;
+			class value_compare
+			{   // in C++98, it is required to inherit binary_function<value_type,value_type,bool>
+				friend class map;
+				protected:
+					Compare comp;
+					value_compare (Compare c) : comp(c) {}  // constructed with map's comparison object
+				public:
+					typedef bool result_type;
+					typedef value_type first_argument_type;
+					typedef value_type second_argument_type;
+
+					bool operator() (const value_type& x, const value_type& y) const
+						{ return comp(x.first, y.first); }
+			};
+
+			// Constructors and Destructor
+			map();
 			
+			explicit map(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type());
+			
+			template <class InputIterator>
+				map(InputIterator first, InputIterator last,
+					const key_compare& comp = key_compare(),
+					const allocator_type& alloc = allocator_type());
+			
+			map(const map& x);
+			~map();
+
+			// Member functions
+			map& operator=(const map& x);
+			iterator begin();
+			const_iterator begin() const;
+			iterator end();
+			const_iterator end() const;
+			reverse_iterator rbegin();
+			const_reverse_iterator rbegin() const;
+			reverse_iterator rend();
+			const_reverse_iterator rend() const;
+			bool empty() const;
+			size_type size() const;
+			size_type max_size() const;
+			mapped_type& operator[](const key_type& k);
+			ft::pair<iterator, bool> insert(const value_type& val);
+			iterator insert(iterator position, const value_type& val);
+			
+			template <class InputIterator>
+				void insert(InputIterator first, InputIterator last);
+
+			void erase(iterator position);
+			size_type erase(const key_type& k);
+			void erase(iterator first, iterator last);
+			void swap(map& x);
+			void clear();
+			key_compare key_comp() const;
+			value_compare value_comp() const;
+			iterator find(const key_type& k);
+			const_iterator find(const key_type& k) const;
+			size_type count(const key_type& k) const;
+			iterator lower_bound(const key_type& k);
+			const_iterator lower_bound(const key_type& k) const;
+			iterator upper_bound(const key_type& k);
+			const_iterator upper_bound(const key_type& k) const;
+			ft::pair<iterator, iterator> equal_range(const key_type& k);
+			ft::pair<const_iterator, const_iterator> equal_range(const key_type& k) const;
 	};
 }
 
--- Vector.hpp
+++ Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: gasselin <gasselin@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 12:49:52 by gasselin          #+#    #+#             */
-/*   Updated: 2022/03/14 14:44:47 by gasselin         ###   ########.fr       */
+/*   Updated: 2022/03/16 17:03:27 by gasselin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,35 +17,73 @@
 
 #include <memory>
 #include <stdbool.h>
+#include <cstddef>
+
+#include "iterators.hpp"
 
 namespace ft
 {
 	template < class T, class Alloc=std::allocator<T> >
 	class vector
-	{
+	{	
 		public:
-			typedef 			T value_type;
-			typedef 			Alloc allocator_type;
-			typedef typename 	allocator_type::reference reference;
-			typedef typename 	allocator_type::const_reference const_reference;
-			typedef typename	allocator_type::pointer pointer;
-			typedef typename	allocator_type::const_pointer const_pointer;
-			typedef				ft::iterator<value_type> iterator;
-			typedef				ft::const_iterator<value_type> const_iterator;
-			typedef				ft::reverse_iterator<iterator> reverse_iterator;
-			typedef				ft::reverse_iterator<const_iterator> const_reverse_iterator;
-			typedef typename	ft::iterator_traits<iterator>::difference_type difference_type;
-			typedef typename	allocator_type::size_type size_type;
+			typedef 			T												value_type;
+			typedef 			Alloc											allocator_type;
+			typedef typename 	allocator_type::reference						reference;
+			typedef typename 	allocator_type::const_reference					const_reference;
+			typedef typename	allocator_type::pointer							pointer;
+			typedef typename	allocator_type::const_pointer					const_pointer;
+			typedef				ft::RandomIterator<value_type>					iterator;
+			typedef				ft::ConstRandomIterator<value_type>				const_iterator;
+			typedef				ft::ReverseRandomIterator<iterator>				reverse_iterator;
+			typedef				ft::ConstReverseRandomIterator<const_iterator>	const_reverse_iterator;
+			typedef				ptrdiff_t										difference_type;
+			typedef				size_t											size_type;
+			
+		private:
+			allocator_type 	_alloc;
+			pointer			_cont_start;
+			pointer			_cont_end;
+			size_type		_cont_size;
 
 			// Constructors and Destructor
-			explicit vector(const allocator_type& alloc = allocator_type());
+			
+			// Note that if a constructor has any arguments that do not
+			// have default values, it is not a default constructor.
+			explicit vector(const allocator_type& alloc = allocator_type())
+				: 	_alloc(alloc),
+					_cont_start(NULL),
+					_cont_end(NULL),
+					_cont_size(0)
+				{}
 
 			explicit vector(size_type n, const value_type& val = value_type(),
 							const allocator_type& alloc = allocator_type());
+				: 	_alloc(alloc),
+					_cont_start(NULL),
+					_cont_end(NULL),
+					_cont_size(n)
+				{
+					_cont_start = _alloc.allocate(n);
+					_cont_end = _cont_start + n;
+					assign(n, val);
+				}
 
 			template <class InputIterator>
 				vector(InputIterator first, InputIterator last,
 						const allocator_type& alloc = allocator_type());
+					: 	_alloc(alloc),
+						_cont_start(NULL),
+						_cont_end(NULL),
+						_cont_size(0)
+					{
+						size_type n;
+						// Calculate distance between the 2 iterators
+						// Verify if InputIterator is an integral type
+						_cont_start = _alloc.allocate(n);
+						_cont_end = _cont_start + n;
+						assign(first, last);
+					}
 
 			vector(const vector& x);
 
@@ -79,7 +117,7 @@ namespace ft
 			template <class InputIterator>
 				void assign(InputIterator first, InputIterator last);
 
-			void assign(size_type n, const value_type& val)
+			void assign(size_type n, const value_type& val);
 			void push_bask(const value_type& val);
 			void pop_back();
 			iterator insert(iterator position, const value_type& val);
@@ -90,30 +128,55 @@ namespace ft
 
 			iterator erase(iterator position);
 			iterator erase(iterator first, iterator last);
-			void swap(vector& x);
+
+			void swap(vector& x)
+			{
+				allocator_type 	tmp_alloc = this->_alloc;
+				pointer			tmp_cont_start = this->_cont_start;
+				pointer			tmp_cont_end = this->_cont_end;
+				size_t			tmp_cont_size = this->_cont_size;
+
+				this->_alloc = x._alloc;
+				this->_cont_start = x._cont_start;
+				this->_cont_end = x._cont_end;
+				this->_cont_size = x._cont_size;
+
+				x._alloc = tmp_alloc;
+				x._cont_start = tmp_cont_start;
+				x._cont_end = tmp_cont_end;
+				x._cont_size = tmp_cont_size;
+			}
+
 			void clear();
 	};
 
 	template <class T, class Alloc>
-		bool operator==(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		bool operator==(const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs)
+			{ return (lhs == rhs); }
 
 	template <class T, class Alloc>
-		bool operator!=(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		bool operator!=(const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs)
+			{ return (lhs != rhs); }
 
 	template <class T, class Alloc>
-		bool operator<(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		bool operator<(const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs)
+			{ return (lhs < rhs); }
 
 	template <class T, class Alloc>
-		bool operator<=(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		bool operator<=(const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs)
+			{ return (lhs <= rhs); }
 
 	template <class T, class Alloc>
-		bool operator>(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		bool operator>(const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs)
+			{ return (lhs > rhs); }
 
 	template <class T, class Alloc>
-		bool operator>=(const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		bool operator>=(const ft::vector<T,Alloc>& lhs, const ft::vector<T,Alloc>& rhs)
+			{ return (lhs >= rhs); }
 
 	template <class T, class Alloc>
-		void swap (vector<T,Alloc>& x, vector<T,Alloc>& y);
+		void swap(ft::vector<T, Alloc>& x,ft:: vector<T, Alloc>& y)
+			{ x.swap(y); }
 }
 
 #endif
